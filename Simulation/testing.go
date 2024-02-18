79cf60d46233a57c92e77b1a7bec7c5e6a8eb5bf
PritibardhanJha2 <2019pgcaca44@nitjsr.ac.in>
2024-02-18 16:24:12 +0530

    DB string changed

--- Simulation/testing.go
+++ Simulation/testing.go
@@ -15,7 +15,7 @@ func Connect() *sql.DB {
 	//dbName := "HistoricalData"
 
 	// Create the connection string
-	connStr := "postgres://priti:Priti@123@localhost:5432/go-server?sslmode=disable"
+	connStr := "postgres://treding_user:LXgxMefi1XjBaaXid87qB0i5Uhoe2GN8@dpg-cn858hmd3nmc73db0v3g-a.oregon-postgres.render.com/treding"
 
 	// Connect to the database
 	db, err := sql.Open("postgres", connStr)
